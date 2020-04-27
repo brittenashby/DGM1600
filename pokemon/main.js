82ff83ecf1687b921a293ba22358c121a0fede43
brittenashby <britten.bell@gmail.com>
2020-04-27 12:27:23 -0600

    added pokemon images and worked on css

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,75 +1,139 @@
-//resusable asnyc function
+let pokeContainer = document.querySelector('.pokeContainer')
+let startButton = document.querySelector('#startButton')
+startButton.addEventListener('click', () => {
+  loadPage()
+})
+
+let newButton = document.querySelector('#newButton')
+newButton.addEventListener('click', () => {
+  const pokemonRect = addPokemon()
+  console.log(pokemonRect)
+  window.scrollTo({
+    top: pokemonRect.top,
+    left: pokemonRect.left,
+    behavior: 'smooth'
+  })
+})
+
 async function getAPIData(url) {
   try {
     const response = await fetch(url)
     const data = await response.json()
     return data
-  } catch (error) {
+  }
+  catch (error) {
     console.error(error)
   }
 }
 
-let pokemonGrid = document.querySelector('.pokemonGrid')
+function loadPage(offset, limit) {
+  getAPIData(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`).then(
+    async (data) => {
+      for (const pokemon of data.results) {
+        await getAPIData(pokemon.url).then(
+          (pokeData) => {
+            populatePokeCard(pokeData)
+          }
+        )
+      }
+    }
+  )
+}
 
-getAPIData('https://pokeapi.co/api/v2/pokemon?&limit=25').then((data) => {
-  for (const pokemon of data.results) {
-    getAPIData(pokemon.url).then((pokeData) => {
-      populatePokeCard(pokeData)
+function populatePokeCard(singlePokemon) {
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener( 'click', function() {
+      pokeCard.classList.toggle('is-flipped')
     })
-  }
-})
-
-populatePokeCard(allPokemon)
-
-
-function populatePokeCard(pokeArray) {
-  pokeArray.forEach((pokemon) => {
-    let pokeScene = document.createElement("div");
-    pokeScene.className = "scene";
-    let pokeCard = document.createElement("div");
-    pokeCard.className = "card";
-    pokeCard.addEventListener("click", () =>
-      pokeCard.classList.toggle("is-flipped")
-    );
     let pokeFront = populateCardFront(singlePokemon)
-    pokeFront.className = "card__face card__face--front";
-    pokeFront.textContent = "front";
-
     let pokeBack = populateCardBack(singlePokemon)
-    pokeBack.className = "card__face card__face--back";
-    pokeBack.textContent = "back";
 
-    pokeCard.appendChild(pokeFront);
-    pokeCard.appendChild(pokeBack);
-    pokeScene.appendChild(pokeCard);
-    pokemonGrid.appendChild(pokeScene);
-  });
+    pokeCard.appendChild(pokeFront)
+    pokeCard.appendChild(pokeBack)
+    pokeScene.appendChild(pokeCard)
+    pokeContainer.appendChild(pokeScene)
+    return pokeScene.getBoundingClientRect()
 }
 
 function populateCardFront(pokemon) {
   let cardFront = document.createElement('div')
   cardFront.className = 'card__face card__face--front'
-  cardFront.textContent = pokemon.name
+  
   let frontImage = document.createElement('img')
-  frontImage.src = `../images/${pokemon.id}.png`
+  frontImage.src = `../images/pokemon-images/${getImageFileName(pokemon)}.png`
+  let frontLabel = document.createElement('p') 
+  frontLabel.textContent = `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`
   cardFront.appendChild(frontImage)
+  cardFront.appendChild(frontLabel)
   return cardFront
 }
 
+function getImageFileName(pokemon) {
+  if (pokemon.id < 10) {
+    return `00${pokemon.id}`
+  } else if (pokemon.id > 9 && pokemon.id < 100) {
+    return `0${pokemon.id}`
+  } else return `pokeball`
+}
+
 function populateCardBack(pokemon) {
-  let cardFront = document.createElement('div')
-  cardFront.className = 'card__face card__face--back'
-  cardFront.textContent = `${pokemon.abilities}`
+  let cardBack = document.createElement('div')
+  cardBack.className = 'card__face card__face--back'
   let abilityList = document.createElement('ul')
-  const abilities = pokemon.abilities.map(ability => {
+  abilityList.textContent = 'Abilities:'
+  pokemon.abilities.forEach(ability => {
     let abilityName = document.createElement('li')
     abilityName.textContent = ability.ability.name
     abilityList.appendChild(abilityName)
   })
+  let moveList = document.createElement('p')
+  moveList.textContent = `Level 0 Moves: ${getPokemonMoves(pokemon, 0).length}`
+  cardBack.appendChild(abilityList)
+  cardBack.appendChild(moveList)
   return cardBack
 }
 
-//also do some formatting over the weekend so the pokemon cards look purty. 
+function getPokemonMoves(pokemon, levelLearnedAt) {
+  return pokemon.moves.filter(move => {
+    return move.version_group_details[0].level_learned_at === levelLearnedAt
+  })
+}
 
+class Pokemon {
+  constructor(height, weight, name, abilities, moves) {
+    this.height = height
+    this.weight = weight
+    this.name = name
+    this.abilities = abilities
+    this.moves = moves
+    this.id = 900
+  }
+}
 
-
+function addPokemon() {
+  let newPokemon = new Pokemon(50, 25, 'CosmiKing', [
+    {
+      ability:
+        { name: 'Space Travel' }
+    },
+    {  ability:
+        { name: 'Time Travel' }
+    }
+  ],
+    [
+      {
+        move: {
+          name: "Teleportation Man"
+        },
+        version_group_details: [
+          {
+          level_learned_at: 0
+          }
+        ]
+      }
+    ])
+  return populatePokeCard(newPokemon)
+}
